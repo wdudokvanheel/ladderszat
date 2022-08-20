ca088d4f562750aa27aaf970df3251c23acca757
wesley <wesley@bitechular.com>
2022-08-20 14:48:54 +0200

    Fixed context destruction which prevented hot reloading

--- ladderzat-game/src/main.ts
+++ ladderzat-game/src/main.ts
@@ -4,25 +4,27 @@ import gameConfig from './GameConfig';
 let game: Phaser.Game;
 
 if (module.hot) {
-	module.hot.dispose(destroyGame);
-	module.hot.accept(newGame);
+	module.hot.dispose(stopGame);
+	module.hot.accept(startGame);
 }
 
 if (!game)
-	newGame();
+	startGame();
 
-function newGame() {
+function startGame() {
 	if (game)
 		return;
 	game = new Phaser.Game(gameConfig);
 }
 
-function destroyGame() {
+function stopGame() {
 	if (!game)
 		return;
+
+	//Context plugin must be destroyed, or it will block next newGame
+	game.plugins.removeGlobalPlugin('context');
 	game.destroy(true);
 	game = null;
 }
 
 export const sample = (arr: any[]) => arr.length ? arr[Math.floor(Math.random() * arr.length)] : undefined
-
